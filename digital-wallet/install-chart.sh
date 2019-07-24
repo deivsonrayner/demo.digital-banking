03e40be6b27a5c30808b41b886530c627b6b4fcd
Deivson Rayner <deivsonrayner@gmail.com>
2019-07-24 16:05:58 -0300

    fix install chart

--- digital-wallet/install-chart.sh
+++ digital-wallet/install-chart.sh
@@ -1,4 +1,4 @@
 echo "INSTALLING CHART: ${1} VERSION: ${2}"
 helm package ./chart/${1}/
 helm delete --purge ${1}
-helm install --name ${1} ${1}/${1}-${2}.tgz
+helm install --name ${1} ${1}-${2}.tgz
